87025031722bab74c3ccb25ee877f053abc53bf4
GxZhu <szhu319@gmail.com>
2020-11-08 23:56:19 -0500

    working model for recog

--- comp-vision/camCapture.m
+++ comp-vision/camCapture.m
@@ -1,5 +1,5 @@
 %% Generate a Webcam Program to Capture Training Data with Image Subtraction
-clear all; close all;
+%clear all; close all;
 cam = webcam(1);
 cam.Resolution = '1280x720';
 
@@ -14,18 +14,23 @@ bkgnd=calibrate(cam,x,y,width,height);
 bkgnd = rgb2gray(bkgnd);
 
 % Where to store images to
-dir = "C:\Users\zhug4\Desktop\Robotics\gestures\";
-class = "02_bunny";
+dir = "C:\Users\zhug4\Desktop\Robotics\gestures\gz\";
+class = "buff";
 
 disp("Live Webcam Feed");
 for idx = 1:2000
     img = snapshot(cam);
+    % Show the webcam feed with the capture area in rectangle
     %img2 = insertShape(img,'Rectangle',[x y width height],'LineWidth',2,'Color','blue');
+    %imshow(img2);
+    
     img1 = img(x:x+height-1,y:y+width-1,:);
     grey = rgb2gray(img1);
     diff = grey-bkgnd;
-    %imshow(img2);
+    rediff = imresize(diff>threshold,[200 256]);
+    disp(classify(net,rediff));
+    %disp(idx);
     imshow(diff>threshold);
-    imwrite(diff>threshold,dir+class+"\"+int2str(idx)+".png");
+    %imwrite(diff>threshold,dir+class+"\"+int2str(idx)+".png");
 end
 disp("Live Webcam Feed End");
---
+++ comp-vision/liveClassify.m
@@ -0,0 +1,42 @@
+%% Generate a Webcam Program to Capture Training Data with Image Subtraction
+%clear all; close all;
+close all; clear all;
+load 4actionmodelNet.mat;
+cam = webcam(1);
+cam.Resolution = '1280x720';
+
+% Rectangle Parameters Trained on 512x400
+x = 50; y = 1; width = 512; height = 400;
+
+% Image subtraction threshold
+threshold = 20;
+
+% Solve for Background Average
+bkgnd=calibrate(cam,x,y,width,height);
+bkgnd = rgb2gray(bkgnd);
+
+% Where to store images to
+%dir = "C:\Users\zhug4\Desktop\Robotics\testData\";
+%class = "vid1";
+
+% Capture Frames
+disp("Live Webcam Feed");
+for idx = 1:500
+    img = snapshot(cam);
+    % Show the webcam feed with the capture area in rectangle
+    img2 = insertShape(img,'Rectangle',[x y width height],'LineWidth',2,'Color','blue');
+    %imshow(img2);
+    
+    img1 = img(x:x+height-1,y:y+width-1,:);
+    grey = rgb2gray(img1);
+    diff = grey-bkgnd;
+    rediff = imresize(diff>threshold,[200 256]);
+    frameGestures(1:idx) = classify(net,rediff);
+    img2 = insertText(img2,[60 5],char(frameGestures(1,idx)));
+    
+    imshow(img2);
+    %imwrite(img2,dir+class+"\"+int2str(idx)+".png");
+end
+disp("Live Webcam Feed End");
+
+clear cam;
